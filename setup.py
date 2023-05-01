06e00e5d80dc64d7f1206ec5f90255dd80e1ec5a
Antibiotic <ofsposs@gmail.com>
2023-05-01 17:05:02 +0200

    added func days_to_birth

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='clean_folder',
+    name='contact-bot',
     version='1',
-    description='Handle contact dictionary',
-    url='https://github.com/Dmytro-Babenko/home-work4',
+    description='console-helper-bot',
+    url='https://github.com/Dmytro-Babenko/console-helper-bot',
     author='Dmytro Babenko',
     author_email='dmytro.babenko87@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['contact-bot = console_bot.main:main']}
+    entry_points={'console_scripts': ['contact-bot = console_bot.__main__:main']}
 )
