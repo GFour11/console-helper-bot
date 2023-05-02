d938c7375d5574eaed1d7897365d90a727f7eed7
Antibiotic <ofsposs@gmail.com>
2023-05-02 12:48:24 +0200

    summary

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(
-    name='contact-bot',
+    name='console-bot',
     version='1',
     description='console-helper-bot',
     url='https://github.com/Dmytro-Babenko/console-helper-bot',
@@ -9,5 +9,5 @@ setup(
     author_email='dmytro.babenko87@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={'console_scripts': ['contact-bot = console_bot.main:main']}
+    entry_points={'console_scripts': ['console-bot = console_bot.main:main']}
 )
