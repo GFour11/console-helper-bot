e5e743ae827aa856df95668a3297e691cdd88640
ulche <ulchegor1@gmaol.com>
2023-05-09 22:28:35 +0300

    one

--- console_bot/main.py
+++ console_bot/main.py
@@ -1,21 +1,49 @@
 import console_bot.ab_work as ab
-from console_bot.handlers import no_command, instruction
+from console_bot.handlers import no_command, instruction, show_all
 import console_bot.Notes as Notes
 import console_bot.sort as sort
+from abc import ABC, abstractmethod
+
 
 MAIN_INSTRUCTION = 'instruction for menu.txt'
 
+class Abstract(ABC):
 
-def help(*args, **kwargs):
-    return instruction(MAIN_INSTRUCTION)
+    @abstractmethod
+    def show_contacts(self):
+        pass
+    @abstractmethod
+    def show_notes(self):
+        pass
+    # @abstractmethod
+    # def help(self):
+    #     pass
+
+
+class Main(Abstract):
+    def show_contacts(self):
+        return show_all()
+    def show_notes(self):
+        return Notes.all([])
+    # def help(self):
+    #     return instruction(MAIN_INSTRUCTION)
+
+
 
 
 def good_bye(*args, **kwargs):
     return 'Good bye'
 
+main = Main()
+
+def help(*args, **kwargs):
+    with open('instruction for menu.txt' 'r') as file:
+        return file.read()
 
 MAIN_COMMANDS = {
     'help': help,
+    "show contacts": main.show_contacts,
+    'show notes': main.show_notes,
     'address book': ab.main,
     'notes': Notes.main,
     'sort': sort.main,
@@ -25,11 +53,12 @@ MAIN_COMMANDS = {
 
 MAIN_COMMANDS_WORDS = '|'.join(MAIN_COMMANDS)
 
-
 def main():
     print(help())
     while True:
-        user_input = input('Choose points: address book, notes, sort: ')
+        user_input = input('Choose points: address book, notes, sort\n'
+                           'Or show contacts, or show notes\n '
+                           'for help print help: ')
         command, _ = ab.parser(user_input, MAIN_COMMANDS_WORDS)
         handler = MAIN_COMMANDS.get(command, no_command)
         output = handler()
